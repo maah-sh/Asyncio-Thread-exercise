923132722080aca1e3732f3c76808c5c5a29327c
Mahnoosh <mahnooshshokri@gmail.com>
2025-10-30 21:33:23 +0330

    Bug fixed in asyncio_gather

--- asyncio_gather.py
+++ asyncio_gather.py
@@ -2,17 +2,18 @@ import asyncio
 import aiofiles
 from datetime import datetime
 
-async def write_output_line(input_line, file):
+async def get_output_line(input_line):
     input_line = input_line.rstrip()
-    new_line = input_line + ", " + str(len(input_line)) + "\n"
-    await file.write(new_line)
+    return input_line + ", " + str(len(input_line)) + "\n"
 
 
 async def main(input_path, output_path):
     async with aiofiles.open(input_path, mode='r') as input_file:
-        async with aiofiles.open(output_path, mode='w') as output_file:
-            input_lines = await input_file.readlines()
-            await asyncio.gather(*(write_output_line(line, output_file) for line in input_lines))
+        input_lines = await input_file.readlines()
+
+    async with aiofiles.open(output_path, mode='w') as output_file:
+        output_lines = await asyncio.gather(*(get_output_line(line) for line in input_lines))
+        await output_file.writelines(output_lines)
 
 
 if __name__ == '__main__':
