deb1a618e73e4f346c351da8cf8b7fcc2ebf764d
Mahnoosh <mahnooshshokri@gmail.com>
2025-10-30 21:46:47 +0330

    Bug fixed

--- asyncio_thread_2.py
+++ asyncio_thread_2.py
@@ -6,16 +6,17 @@ def get_output_line(input_line):
     input_line = input_line.rstrip()
     return input_line + ", " + str(len(input_line)) + "\n"
 
-async def write_output_line(input_line, file):
-    output_line = await asyncio.to_thread(get_output_line,input_line)
-    await file.write(output_line)
+async def async_output_line(input_line):
+    return await asyncio.to_thread(get_output_line,input_line)
 
 
 async def main(input_path, output_path):
     async with aiofiles.open(input_path, mode='r') as input_file:
-        async with aiofiles.open(output_path, mode='w') as output_file:
-            input_lines = await input_file.readlines()
-            await asyncio.gather(*(write_output_line(line, output_file) for line in input_lines))
+        input_lines = await input_file.readlines()
+
+    async with aiofiles.open(output_path, mode='w') as output_file:
+        output_lines = await asyncio.gather(*(async_output_line(line) for line in input_lines))
+        await output_file.writelines(output_lines)
 
 
 if __name__ == '__main__':
